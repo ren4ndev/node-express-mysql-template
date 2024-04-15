fa0e82bd7c1827d64f69ed715611327e340b4530
Renan Oliveira <oliveira.renan.rg@gmail.com>
2024-04-15 15:06:43 -0300

    feat: Adds security measures

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,21 @@ import express from "express";
 import { Request, Response } from "express";
 import { connection } from "@/db/connection";
 import dotenv from "dotenv";
+import cors, { CorsOptions } from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.disable("x-powered-by");
+
+const allowedHosts = process.env.ENABLED_HOSTS?.split(";") || [];
+
+const options: CorsOptions = {
+  origin: allowedHosts,
+};
+
+app.use(cors(options));
 
 app.get("/", (_req: Request, res: Response) =>
   res.status(200).json({ message: "Hello World" }),
